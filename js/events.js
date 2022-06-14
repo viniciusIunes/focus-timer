9013dceee048f8e4209251733091f5cb353578a7
Vinícius Moreira <64819321+viniciusIunes@users.noreply.github.com>
2022-06-14 16:48:12 -0300

    Update events.js

--- js/events.js
+++ js/events.js
@@ -17,18 +17,27 @@ export default function ({
     controls.play()
     timer.coutdown()
     sound.pressButton()
+    sound.bgAudio.play()
+    buttonSoundOn.classList.remove('hide')
+    buttonSoundOff.classList.add('hide')
   })
 
   buttonPause.addEventListener('click', function () {
     controls.pause()
     timer.hold()
     sound.pressButton()
+    sound.bgAudio.pause()
+    buttonSoundOff.classList.remove('hide')
+    buttonSoundOn.classList.add('hide')
   })
 
   buttonStop.addEventListener('click', function () {
     controls.reset()
     timer.reset()
     sound.pressButton()
+    sound.bgAudio.pause()
+    buttonSoundOff.classList.remove('hide')
+    buttonSoundOn.classList.add('hide')
   })
 
   buttonSoundOff.addEventListener('click', function () {
